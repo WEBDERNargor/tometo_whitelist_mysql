c9f0fe477309f81437ce0b12d1b2b37a202b1d9a
WEBDER <tomditsarut2@gmail.com>
2022-04-02 17:04:33 +0700

    tom001

---
+++ server.lua
@@ -0,0 +1,37 @@
+
+local whitelist={
+    "steam:5555",
+    "steam:110000113c5b36d",
+}
+AddEventHandler("playerConnecting", function(name, setKickReason, deferrals)
+    local player = source
+    local identifiers = GetPlayerIdentifiers(player)
+    local steam
+    local allow=false
+    deferrals.defer()
+
+    -- mandatory wait!
+    Wait(0)
+    for _, v in pairs(identifiers) do
+        if string.find(v, "steam") then
+            steam = v
+            break
+        end
+    end
+
+    for _,data in pairs(whitelist) do
+        if steam==data then
+            allow=true
+        end
+    end
+    deferrals.update(string.format("สวัสดีคุณ %s ครับ", name))
+
+    if allow==false then 
+        deferrals.done("คุณไม่มีไวรีส "..steam)
+    else 
+        deferrals.done()
+    end
+    -- mandatory wait!
+    Wait(0)
+
+end)
